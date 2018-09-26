053b8c7f0d79f7693dfaf90502499714c3cac667
yusukemorita <yusuke.ppq@gmail.com>
2018-09-26 14:42:27 +0900

    removed unneeded mechanize user agent settings

--- fb_token_retriever.rb
+++ fb_token_retriever.rb
@@ -5,9 +5,6 @@ LOGIN_URL = 'https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386
             'nship_details,user_friends,user_work_history,user_likes&response_type=token%2Csigned' \
             '_request&client_id=464891386855067'.freeze
 
-USER_AGENT = 'Mozilla/5.0 (Linux; U; en-gb; KFTHWI Build/JDQ39) AppleWebKit/535.19 (KHTML' \
-             ', like Gecko) Silk/3.16 Safari/535.19'.freeze
-
 def get_fb_auth_token
   if ARGV.count != 2
     puts 'Wrong number of args. Usage example: ruby get_fb_auth_token.rb [email] [password]'
@@ -15,8 +12,6 @@ def get_fb_auth_token
   end
 
   mechanize = Mechanize.new
-  mechanize.user_agent = USER_AGENT
-
   login_form = mechanize.get(LOGIN_URL).form do |f|
     f.email = ARGV[0]
     f.pass = ARGV[1]
