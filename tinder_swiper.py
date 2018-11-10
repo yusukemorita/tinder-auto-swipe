0c310d2880cfc96d47cdfb4c375d4b8ae84f4a3d
yusukemorita <yusuke.ppq@gmail.com>
2018-11-10 18:18:19 +0900

    added error handling in python script

--- tinder_swiper.py
+++ tinder_swiper.py
@@ -5,18 +5,22 @@ def main():
     token = sys.argv[1]
     session = pynder.Session(token)
     swipe_count = 0
+    match_count = 0
 
-    for user in session.nearby_users():
-        try:
-            print(user.name)
-            swipe_result = user.like()
-            ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
-            print(f'{ordinal(swipe_count + 1)} person liked')
-            if swipe_result == True:
-                print("---------- IT'S A MATCH!!!----------")
-            print("")
-            swipe_count += 1
-        except:
-            print("unexpected error, skipping")
+    try:
+        for user in session.nearby_users():
+            try:
+                swipe_result = user.like()
+                swipe_count += 1
+                if swipe_result == True: match_count += 1
+                print(f'{swipe_count} {user.name}')
+            except:
+                print("unexpected error inside for loop, skipping")
+    except pynder.errors.RecsTimeout:
+        print("There are no more users left!")
+    except:
+        print("unexpected error, skipping")
+    finally:
+        print(f'{swipe_count} users swiped, {match_count} matches found')
 
 main()
